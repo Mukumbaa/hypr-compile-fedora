7edc1421508d501b085250b9951f399a9ca5aa67
Mukumbaa <gabrylippolis@gmail.com>
2026-09-14 14:33:03 +0200

    adding yazi

--- script.lua
+++ script.lua
@@ -100,6 +100,12 @@ local all_modules = {
   { 
     url = "https://github.com/outfoxxed/quickshell.git",               dir = "quickshell",                  extra_args = "-DVENDOR_CPPTRACE=ON -DINSTALL_QML_PREFIX=lib64/qt6/qml",
     build_reqs = "qt6-qtbase-devel qt6-qtbase-private-devel qt6-qtdeclarative-devel qt6-qtwayland-devel qt6-qtshadertools-devel qt6-qtsvg-devel cli11-devel jemalloc-devel pipewire-devel libdrm-devel mesa-libGL-devel vulkan-headers polkit-devel libxcb-devel libunwind-devel libdwarf-devel", core_deps = {} 
+  },
+  { 
+    url = "https://github.com/sxyazi/yazi.git", 
+    dir = "yazi", 
+    build_reqs = "cargo rustc", 
+    core_deps = {} 
   }
 }
 
@@ -231,6 +237,8 @@ if [ -f "CMakeLists.txt" ]; then
 elif [ -f "meson.build" ]; then
   %%meson %s
   %%meson_build
+elif [ -f "Cargo.toml" ]; then
+  cargo build --release --locked
 fi
     
 %%install
@@ -238,6 +246,9 @@ if [ -f "CMakeLists.txt" ]; then
   %%cmake_install
 elif [ -f "meson.build" ]; then
   %%meson_install
+elif [ -f "Cargo.toml" ]; then
+  install -Dm755 target/release/yazi %%{buildroot}%%{_bindir}/yazi
+  install -Dm755 target/release/ya %%{buildroot}%%{_bindir}/ya
 fi
 
 rm -rf %%{buildroot}%%{_libdir}/cmake/zstd %%{buildroot}%%{_libdir}/pkgconfig/libdwarf.pc %%{buildroot}%%{_libdir}/pkgconfig/libzstd.pc
