391717bde9ecc211022d608584c7bec72d2bb613
Mukumbaa <gabrylippolis@gmail.com>
2026-09-21 09:38:09 +0200

    updates

--- script.lua
+++ script.lua
@@ -110,7 +110,7 @@ local all_modules = {
   {
     url = "https://github.com/kovidgoyal/kitty.git",
     dir = "kitty",
-    build_reqs = "golang python3-devel harfbuzz-devel libpng-devel dbus-devel wayland-devel wayland-protocols-devel libxkbcommon-devel xxhash-devel simde-devel",
+    build_reqs = "golang python3-devel harfbuzz-devel libpng-devel dbus-devel wayland-devel wayland-protocols-devel libxkbcommon-devel xxhash-devel simde-devel openssl-devel",
     core_deps = {}
   }
 }
