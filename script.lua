158965a66fb8ac2f8e1ff4f35bdf1cd8c527efdf
Mukumbaa <gabrylippolis@gmail.com>
2026-09-21 11:04:34 +0200

    updates

--- script.lua
+++ script.lua
@@ -110,7 +110,7 @@ local all_modules = {
 {
     url = "https://github.com/kovidgoyal/kitty.git",
     dir = "kitty",
-    build_reqs = "golang python3-devel ncurses libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libxkbcommon-devel dbus-devel fontconfig harfbuzz-devel zlib-devel slang slang-devel xxhash-devel",
+    build_reqs = "golang python3-devel ncurses libX11-devel libXrandr-devel libXinerama-devel libXcursor-devel libxkbcommon-devel dbus-devel fontconfig harfbuzz-devel zlib-devel slang slang-devel xxhash-devel openssl-devel",
     core_deps = {}
   }
 }
