faad0f6fb3c82537e0e041eb1e1de7092ba8d876
Mukumbaa <gabrylippolis@gmail.com>
2026-09-21 10:30:23 +0200

    updates

--- script.lua
+++ script.lua
@@ -247,7 +247,7 @@ elif [ -f "Cargo.toml" ]; then
   cargo build --release --locked ${CARGO_BUILD_JOBS:+-j $CARGO_BUILD_JOBS}
 elif [ -f "setup.py" ]; then
   export CFLAGS="$CFLAGS -Wno-error=format-truncation -Wno-format-truncation"
-  make linux-package
+  python3 setup.py linux-package --no-vcs
 fi
     
 %%install
